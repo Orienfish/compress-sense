9898269b535dad107f4e9e1debb31ad7ad6aa6e5
Orienfish <x1yu@ucsd.edu>
2020-06-04 10:41:14 -0700

    implement the CVX solution

--- main.m
+++ main.m
@@ -16,7 +16,7 @@ warning('off','all')
     
     % ramdomly generate the s-sparse signal with length n
 %    comb = combnk(1:n, s);
-%    sig_cnt = size(comb, 1);   % count of potential signals in K
+%    sig_cnt = size(comb, 1);   % length of signal
 %    sig = zeros(sig_cnt, n);
 %    sig1 = zeros(sig_cnt, n);
 %    sig2 = zeros(sig_cnt, n);
@@ -49,7 +49,7 @@ warning('off','all')
 % together with lower and upper bound
 %figure(1);
 %c = 3.0;
-C = 4.5;
+%C = 4.5;
 %lb = sqrt(c * s * log(2*n_list/s));
 %ub = sqrt(C * s * log(2*n_list/s));
 %plot(n_list, mwid_s, '-*', 'DisplayName', 'w(S)', 'LineWidth', 2);
@@ -76,10 +76,45 @@ C = 4.5;
 %ax = gca(); ax.FontSize = 16;
 
 %% Use the convex program to decode the sensing data
-% determine m
-s = 5;
-n = 20;
-delta = 0.1;
-m = ceil(C * delta^(-2) * s * log(2*n/s));
-% randomly generate A
+% fundamental parameters
+s = 5;                      % sparsity level
+n = 20;                     % length of signal
+delta = 0.1;                % approximation level of using CVX
+rou = 0.4;                  % flip probability in the noise model
+C = 4.5;                    % constant in determine the lower bound of m
+m = ceil(C * delta^(-2) * (rou - 0.5)^(-2) * ...
+    s * log(2*n/s));        % required observation number
+%lambda = 2 * sqrt(2 / pi) * (rou - 0.5);    % lambda for the noise model
+
+% ramdomly generate the s-sparse signal with length n
+comb = combnk(1:n, s);
+% randomly select one comb as x
+x = zeros(n, 1);
+comb = comb(round(rand()*size(comb, 1)), :);
+for k=1:s
+    x(comb(k)) = -1 + 2*rand();
+end
+% normalize if necessary
+if norm(x) > 1
+    x = x / norm(x);
+end
+
+% randomly generate A and the observation y
 A = -1 + 2 * rand(m, n);
+y = A * x;                  % true observation
+y_c = y;                    % corrupted observation
+flip_cnt = 0;
+for i=1:m
+    if rand() < rou
+        y_c(i) = y(i) * (-1); % flip the sign of y with probability rou
+        flip_cnt = flip_cnt + 1;
+    end
+end
+cvx_begin
+    variable x_p(n)
+    maximize( y_c' * A * x_p )
+    subject to
+        abs( x_p ) <= sqrt(s);
+        norm( x_p ) <= 1;
+cvx_end
+err_cvx = norm(x - x_p)^2;
