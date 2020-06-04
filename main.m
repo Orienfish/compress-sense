cedbe4bb73b89506736f5b5daff5a11295b95ec2
Orienfish <x1yu@ucsd.edu>
2020-06-04 10:43:59 -0700

    tune C

--- main.m
+++ main.m
@@ -79,9 +79,9 @@ warning('off','all')
 % fundamental parameters
 s = 5;                      % sparsity level
 n = 20;                     % length of signal
-delta = 0.1;                % approximation level of using CVX
+delta = 0.01;                % approximation level of using CVX
 rou = 0.4;                  % flip probability in the noise model
-C = 4.5;                    % constant in determine the lower bound of m
+C = 0.006;                    % constant in determine the lower bound of m
 m = ceil(C * delta^(-2) * (rou - 0.5)^(-2) * ...
     s * log(2*n/s));        % required observation number
 %lambda = 2 * sqrt(2 / pi) * (rou - 0.5);    % lambda for the noise model
@@ -110,11 +110,13 @@ for i=1:m
         flip_cnt = flip_cnt + 1;
     end
 end
+
+% call CVX
 cvx_begin
     variable x_p(n)
     maximize( y_c' * A * x_p )
     subject to
-        abs( x_p ) <= sqrt(s);
-        norm( x_p ) <= 1;
+        abs( x_p ) <= sqrt(s)
+        norm( x_p ) <= 1
 cvx_end
 err_cvx = norm(x - x_p)^2;
